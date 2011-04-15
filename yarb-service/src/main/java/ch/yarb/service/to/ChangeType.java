995de8db373ab45af72d5f08dbe42a6fbbeb4f16
pellaton <michael.pellaton@gmail.com>
2011-04-15 23:49:46 +0200

    - added javadoc

--- yarb-service/src/main/java/ch/yarb/service/to/ChangeType.java
+++ yarb-service/src/main/java/ch/yarb/service/to/ChangeType.java
@@ -1,9 +1,21 @@
 package ch.yarb.service.to;
 
-
+/**
+ * Enumeration representing the type of change of a path in the repository.
+ *
+ * @author pellaton
+ */
 public enum ChangeType {
+
+  /** The path was added. */
   ADDED,
+
+  /** The path was deleted. */
   DELETED,
+
+  /** The path was modified. */
   MODIFIED,
+
+  /** The path was replaced. */
   REPLACED
 }
