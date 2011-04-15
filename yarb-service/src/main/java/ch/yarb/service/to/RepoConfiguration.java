bb8bcada1fb33a65dce0a3ea87f86e6dc5dd43a4
pellaton <michael.pellaton@gmail.com>
2011-04-15 23:55:29 +0200

    - add javadoc comments

--- yarb-service/src/main/java/ch/yarb/service/to/RepoConfiguration.java
+++ yarb-service/src/main/java/ch/yarb/service/to/RepoConfiguration.java
@@ -1,28 +1,54 @@
 package ch.yarb.service.to;
 
-
+/**
+ * Object representing a repository configuration.
+ *
+ * @author pellaton
+ */
 public class RepoConfiguration {
 
   private final String repoUrl;
   private final String userName;
   private final String password;
 
+
+  /**
+   * Constructor.
+   *
+   * @param repoUrl the repository URL
+   * @param userName the user name
+   * @param password the password
+   */
   public RepoConfiguration(String repoUrl, String userName, String password) {
     this.repoUrl = repoUrl;
     this.userName = userName;
     this.password = password;
   }
 
+  /**
+   * Gets the repository url.
+   *
+   * @return the repository url
+   */
   public String getRepoUrl() {
     return this.repoUrl;
   }
 
+  /**
+   * Gets the user name.
+   *
+   * @return the user name
+   */
   public String getUserName() {
     return this.userName;
   }
 
+  /**
+   * Gets the password.
+   *
+   * @return the password
+   */
   public String getPassword() {
     return this.password;
   }
-
 }
