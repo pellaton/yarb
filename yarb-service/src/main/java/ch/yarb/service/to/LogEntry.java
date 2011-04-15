1e37327952effe3adee9f26494c4e4949b1ea5e6
pellaton <michael.pellaton@gmail.com>
2011-04-15 23:52:53 +0200

    - add some javadoc

--- yarb-service/src/main/java/ch/yarb/service/to/LogEntry.java
+++ yarb-service/src/main/java/ch/yarb/service/to/LogEntry.java
@@ -5,7 +5,11 @@ import java.util.List;
 
 import org.joda.time.DateTime;
 
-
+/**
+ * An object representing a single entry in the repository log.
+ *
+ * @author pellaton
+ */
 public class LogEntry implements Serializable {
 
   private final String revision;
@@ -14,8 +18,17 @@ public class LogEntry implements Serializable {
   private final String comment;
   private final List<ChangedPath> changedPathList;
 
-
-  public LogEntry(String revision, DateTime timestamp, String author, String comment, List<ChangedPath> changedPathList) {
+  /**
+   * Constructor.
+   *
+   * @param revision the revision identifier
+   * @param timestamp the timestamp
+   * @param author the author
+   * @param comment the comment
+   * @param changedPathList the list of changed paths
+   */
+  public LogEntry(String revision, DateTime timestamp, String author, String comment,
+      List<ChangedPath> changedPathList) {
     this.revision = revision;
     this.timestamp = timestamp;
     this.author = author;
@@ -24,24 +37,48 @@ public class LogEntry implements Serializable {
   }
 
 
+  /**
+   * Gets the revision identifier.
+   *
+   * @return the revision identifier
+   */
   public String getRevision() {
     return this.revision;
   }
 
+  /**
+   * Gets the timestamp.
+   *
+   * @return the timestamp
+   */
   public DateTime getTimestamp() {
     return this.timestamp;
   }
 
+  /**
+   * Gets the author.
+   *
+   * @return the author
+   */
   public String getAuthor() {
     return this.author;
   }
 
+  /**
+   * Gets the comment.
+   *
+   * @return the comment
+   */
   public String getComment() {
     return this.comment;
   }
 
+  /**
+   * Gets the list of changed paths.
+   *
+   * @return the list of changed paths
+   */
   public List<ChangedPath> getChangedPathList() {
     return this.changedPathList;
   }
 }
-
