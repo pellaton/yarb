fcad7b9019092a35c5f09c25f8a87023975c961a
Martin Roth <martin.roth@access.sh>
2011-04-16 15:22:49 +0200

    adding dummy service for testing

---
+++ yarb-gwt/src/main/java/ch/yarb/gwt/server/YarbDummyService.java
@@ -0,0 +1,37 @@
+package ch.yarb.gwt.server;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.joda.time.DateTime;
+
+import ch.yarb.api.service.YarbService;
+import ch.yarb.api.to.ChangedPath;
+import ch.yarb.api.to.LogEntry;
+import ch.yarb.api.to.RepoConfiguration;
+import ch.yarb.api.to.RevisionRange;
+
+
+/**
+ * Dummy implementation of the {@link YarbService}.
+ */
+public class YarbDummyService implements YarbService {
+
+  @Override
+  public String ping() {
+    // TODO Auto-generated method stub
+    return "yarb-dummy";
+  }
+
+  @Override
+  public List<LogEntry> getRepositoryLog(RepoConfiguration repoConfiguration,
+      RevisionRange revisionRange) {
+    List<LogEntry> log = new ArrayList<LogEntry>();
+    log.add(new LogEntry("1", new DateTime(), "mro", "foo", new ArrayList<ChangedPath>()));
+    log.add(new LogEntry("2", new DateTime(), "mro", "bar", new ArrayList<ChangedPath>()));
+    log.add(new LogEntry("3", new DateTime(), "mro", "less", new ArrayList<ChangedPath>()));
+
+    return log;
+  }
+
+}
