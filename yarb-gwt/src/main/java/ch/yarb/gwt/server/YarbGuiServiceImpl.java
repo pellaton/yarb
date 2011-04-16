2362264b0437dc715188364dfb75b879ad4628f0
Martin Roth <martin.roth@access.sh>
2011-04-16 12:02:39 +0200

    refactor logging

--- yarb-gwt/src/main/java/ch/yarb/gwt/server/YarbGuiServiceImpl.java
+++ yarb-gwt/src/main/java/ch/yarb/gwt/server/YarbGuiServiceImpl.java
@@ -3,6 +3,8 @@ package ch.yarb.gwt.server;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.BeanFactoryUtils;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
@@ -11,30 +13,27 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 import ch.yarb.gwt.client.YarbGuiService;
 import ch.yarb.service.api.YarbService;
+import ch.yarb.service.impl.YarbServiceImpl;
 
 public class YarbGuiServiceImpl extends RemoteServiceServlet implements YarbGuiService {
+
+  private static final Logger LOG = LoggerFactory.getLogger(YarbServiceImpl.class);
 
   private transient WebApplicationContext applicationContext;
   private transient YarbService yarbService;
 
-
-  public YarbGuiServiceImpl() {
-    System.out.println("##################### hello world");
-  }
 
   @Override
   public void init(ServletConfig config) throws ServletException {
     // TODO Auto-generated method stub
     super.init(config);
 
-    System.out.println("initializing the gui service.. ");
-
     // spring application context
     this.applicationContext = WebApplicationContextUtils.getWebApplicationContext(config.getServletContext());
     this.yarbService = BeanFactoryUtils.beanOfType(this.applicationContext, YarbService.class);
 
-    System.out.println("appContext: " + this.applicationContext);
-    System.out.println("yarbService: " + this.yarbService);
+    LOG.info("initialized yarbGuiService - appContext: {}, yarbService: {}",
+        this.applicationContext, this.yarbService);
   }
 
   public String ping() {
