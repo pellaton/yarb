df232655a7703f849bf2e7f7c34ba7a9e93d28d2
Martin Roth <martin.roth@access.sh>
2011-04-16 21:44:42 +0200

    refactored gwt module, should work soon ;-)

--- yarb-gwt/src/main/java/ch/yarb/gwt/client/YarbGuiServiceAsync.java
+++ yarb-gwt/src/main/java/ch/yarb/gwt/client/YarbGuiServiceAsync.java
@@ -5,10 +5,12 @@ import java.util.List;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.rpc.RemoteService;
 
+import ch.yarb.api.to.LogEntry;
+
 public interface YarbGuiServiceAsync extends RemoteService {
 
   void ping(AsyncCallback<String> callback);
 
-  void getRepositoryLog(AsyncCallback<List<String>> callback);
+  void getRepositoryLog(String url, AsyncCallback<List<LogEntry>> callback);
 
 }
